6db6f76e173b9d52e12026234d05d6b10d2f3eb1
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-05 16:31:33 +0530

    [Akhil] Uc2

--- EmployeePayrollMultiThreading/EmployeeRepository.cs
+++ EmployeePayrollMultiThreading/EmployeeRepository.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace EmployeePayrollMultiThreading
 
 {
     public class EmployeeRepository
-    { 
+    {
         public static string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Payroll_Service_ADO_NET;Integrated Security=True";
-        SqlConnection connection = new SqlConnection(connectionString);  
+        SqlConnection connection = new SqlConnection(connectionString);
 
         public bool DataBaseConnection()
         {
             try
             {
-                DateTime now = DateTime.Now; 
-                connection.Open(); 
-                using (connection)  
+                DateTime now = DateTime.Now;
+                connection.Open();
+                using (connection)
                 {
-                    Console.WriteLine($"Connection is created Successful {now}"); 
+                    Console.WriteLine($"Connection is created Successful {now}");
 
                 }
                 connection.Close();
@@ -43,14 +45,30 @@ namespace EmployeePayrollMultiThreading
             }
             return true;
         }
+         //UC2:- Ability to add multiple employee to payroll DB using Threads so as to get a better response
+        public void AddEmployeeListToEmployeePayrollDataBaseWithThread(List<EmployeeModel> employeelList)
+        {
+            employeelList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Console.WriteLine("Employee Being added" + employeeData.EmployeeName); 
+                    Console.WriteLine("Current thread id: " + Thread.CurrentThread.ManagedThreadId); 
+                    this.AddEmployeeToDataBase(employeeData);
+                    Console.WriteLine("Employee added:" + employeeData.EmployeeName);
+                });
+                thread.Start();
+            });
+        }
         public bool AddEmployeeToDataBase(EmployeeModel model)
         {
             try
             {
                 using (connection)
                 {
-                    SqlCommand command = new SqlCommand("dbo.SqlProcedureName", this.connection);   
-                    command.CommandType = CommandType.StoredProcedure;
+                    SqlCommand command = new SqlCommand("dbo.SqlProcedureName", this.connection);  
+                    command.CommandType = CommandType.StoredProcedure; 
+
                     command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
                     command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
                     command.Parameters.AddWithValue("@Address", model.Address);
@@ -83,8 +101,6 @@ namespace EmployeePayrollMultiThreading
             {
                 connection.Close();
             }
-
         }
-
     }
 }
